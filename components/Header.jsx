1b1543428fbcdc239cf477cebcd4a6cdc7d56255
rohxn-rai <rohan.rai932@outlook.com>
2025-04-03 06:53:54 -0400

    modified Header - changes the styling to adjust with multiple screens

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,13 +7,13 @@ const Header = () => {
       <div className="container mx-auto px-5 flex justify-between items-center">
         <div className="hidden md:flex md:flex-row gap-6">
           <Image src="/box.png" height={50} width={50} alt="Box Icon" />
-          <h1 className="text-5xl pt-0.5">Click Box Game</h1>
+          <h1 className="text-4xl pt-2.5">Click Box Game</h1>
           <span className="text-white/40 pt-5">By Rohan Rai</span>
         </div>
         <div className="flex md:hidden md:flex-row gap-3">
           <Image src="/box.png" height={25} width={40} alt="Box Icon" />
-          <h1 className="text-3xl pt-1">Click Box Game</h1>
-          <span className="text-white/40 pt-3">By Rohan Rai</span>
+          <h1 className="text-3xl pt-2">Click Box Game</h1>
+          <span className="text-white/40 pt-3.5">By Rohan Rai</span>
         </div>
       </div>
     </header>
