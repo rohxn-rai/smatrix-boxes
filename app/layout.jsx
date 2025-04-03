4e869eaf43c91fa9025b3b89bc36d6035aee8d41
rohxn-rai <rohan.rai932@outlook.com>
2025-04-03 06:33:51 -0400

    modified layout - added Header - added Footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
@@ -15,7 +18,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
