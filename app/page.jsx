7c056fca29f16e6170ba0c03f8426a53711d3d06
rohxn-rai <rohan.rai932@outlook.com>
2025-04-03 05:18:21 -0400

    modified Home - added a matrix boxes layout that changes colour to green when clicked - added a restart button (non-functional)

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+
+import { FaArrowRotateLeft } from "react-icons/fa6";
 
 const Home = () => {
-  return <div>Home</div>;
+  const initialMatrix = Array.from({ length: 3 }, () => Array(3).fill(false));
+  const [matrix, setMatrix] = useState(initialMatrix);
+
+  const handleClick = (row, col) => {
+    const newMatrix = matrix.map((r, i) =>
+      r.map((cell, j) => (i === row && j === col ? true : cell))
+    );
+    setMatrix(newMatrix);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen gap-16">
+      <div className="grid grid-cols-3 gap-4">
+        {matrix.map((row, rowIndex) =>
+          row.map((isGreen, colIndex) => (
+            <div
+              key={`${rowIndex}-${colIndex}`}
+              onClick={() => handleClick(rowIndex, colIndex)}
+              className={`w-24 h-24 border flex justify-center items-center cursor-pointer transition-colors duration-300
+            ${isGreen ? "bg-green-500" : "bg-white"}`}
+            ></div>
+          ))
+        )}
+      </div>
+      <div className="flex flex-row gap-3">
+        <FaArrowRotateLeft className="text-3xl mt-1" />
+        <span className="text-4xl">Restart</span>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
